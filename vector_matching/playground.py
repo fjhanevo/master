d7b6688e7e81d2cf70267062479e4d4fea67221b
fjhanevo <fredrik.hanevold@gmail.com>
2025-04-01 13:08:31 +0200

    cleaned up

--- vector_matching/playground.py
+++ vector_matching/playground.py
@@ -40,100 +40,24 @@ def make_rotating_sphere_gif(vec1:np.ndarray,vec2:np.ndarray,labels:tuple) -> No
 
 
 
-def run_full_tree(experimental, simulated, reciprocal_radius, ang_step):
-    in_plane = []
-    for idx in range(experimental.shape[0]):
-        # convert 2D to 3D
-        frame = np.array(sm.vector_to_3D(experimental[idx], reciprocal_radius))
-        # fully rotate the frame
-        rot_frame = sm.full_z_rotation(frame, ang_step)
-        # debuggggggggggg
-        print(rot_frame.shape)
-
-        # now we can use kdtrees to match
-        ## WILL only track rotation this time!
-        _,_, rot = sm.kdtree_match(rot_frame, simulated, ang_step)
-        
-        # save rot at each iteration
-        in_plane.append(rot)
-
-    # assuming this takes a long time to run so will save in_plane
-    in_plane = np.array(in_plane)
-    np.save(file='full_in_plane_test.npy',arr=in_plane,allow_pickle=True)
-    print("We did it!")
-
-def match_one_frame(exp, sim, ang_step):
+def match_one_frame(exp, sim, ang_step, reciprocal_radius, n_best):
     t1 = time()
     # First apply a full rotation to the experimental frame
     # exp = sm.full_z_rotation(exp,ang_step)
     # frames, score, rotation = sm.kdtree_match(exp,sim,ang_step)
     # frames, score, rotation = sm.kdtree_sim_rot(exp,sim,ang_step)
     # frames, score, rotation = sm.fast_kdtree_sim_rot(exp,sim,ang_step)
-    frames, score, rotation = sm.slightly_fast_kdtree(exp,sim,ang_step)
+    frames, score, rotation = sm.vector_match(exp,sim,ang_step,reciprocal_radius, n_best)
     t2 = time()
     print(f"Computation time: {(t2-t1)/60} min")
     print('Best frames:', frames)
     print('Best score:', score)
     print('Rotation:', rotation)
 
-def create_result_dataset(exp, sim, ang_step, reciprocal_radius):
-    result_lst = []
 
-    # First turn exp and sim to 3D
-    sim3d = np.array([sm.vector_to_3D(sim_vec,reciprocal_radius) for sim_vec in sim])
-    # We do it like this cause its inhomogeneous
-    for exp_vec in exp:
-        exp3d = np.array(sm.vector_to_3D(exp_vec, reciprocal_radius))
-        # Apply a full z-rotation to each frame
-        exp3d = sm.full_z_rotation(exp3d,ang_step)
-        frame, score, rotation = sm.kdtree_match(exp3d,sim3d,ang_step)
-        # Create nx4 array ['index', 'correlation', 'in-plane', mirror_factor]
-        lst = np.array([[frame, score, rotation, 1.0]])
-        # Append the data 
-        result_lst.append(lst)
-    n_array = np.array(result_lst)
-    print(n_array.shape)
-    return n_array
-
-def full_create_dataset(exp, sim, ang_step, reciprocal_radius):
-    results = []
-
-    # Convert sim to 3D
-    sim3d = np.array([sm.vector_to_3D(sim_vec, reciprocal_radius) for sim_vec in sim])
-
-    for exp_vec in exp:
-        # Convert to 3D like this cause its inhomogeneous
-        exp3d = np.array(sm.vector_to_3D(exp_vec, reciprocal_radius))
-        # Apply a full z-rotation
-        exp3d = sm.full_z_rotation(exp3d, ang_step)
-
-        # Track best matches for all simulated frames
-        frames, scores, rotations = sm.full_kdtree_match(exp3d, sim3d,ang_step)
-
-        # Create nx4 array ['index', 'correlation', 'in-plane', mirror_factor]
-        lst = np.column_stack((frames, scores, rotations, np.ones_like(frames)))
-        results.append(lst)
-    n_array = np.array(results)
-    print(n_array.shape)
-    return n_array
-
-def alt_create_dataset(exp, sim, ang_step, reciprocal_radius):
-    results = []
-
-    # Rotate sim3d 
-    sim3d = np.array([sm.vector_to_3D(sim_vec,reciprocal_radius) for sim_vec in sim])
-
-    for exp_vec in exp:
-        exp3d = np.array(sm.vector_to_3D(exp_vec, reciprocal_radius))
-        # Apply a full z-rotation to each frame
-        exp3d = sm.full_z_rotation(exp3d,ang_step)
-        frame, score, rotation = sm.kdtree_match(exp3d,sim3d,ang_step)
-        lst = np.column_stack((frame, score, rotation, np.ones_like(frame)))
-        results. append(lst)
-    n_array = np.array(results)
-    print(n_array.shape)
-    return n_array
-
+def create_and_save_dataset(experimental, simulated, ang_step, reciprocal_radius, n_best, filename:str):
+    n_array = sm.vector_match(experimental, simulated, ang_step, reciprocal_radius, n_best)
+    np.save(file=filename, arr=n_array, allow_pickle=True)
 
 
 if __name__ == '__main__':
@@ -141,34 +65,57 @@ if __name__ == '__main__':
     FILE_EXP = 'LF_peaks_m_center_m_peaks.npy'
     FILE_STRICT = 'LF_strict_peaks_log.npy'
     FILE_SIM = 'LF_r_theta_sim.npy'
-    # IN_PLANE_FILE = 'full_in_plane_test.npy'
-    # IN_PLANE_FILE = 'ang_step0_01_factor1_negative_rot.npy'
+    # FILE_SIM = 'filtered_simulation.npy'
+    # FILE_SIM_ROT = 'sim_ang0005.npy'
+    FILE_SIM_ROT = 'sim_rot_ang001.npy'
+    FILE_SIM_ROT2 = 'sim_ang0005.npy'
     IN_PLANE_FILE = 'test_vector_match_ang0005.npy'
 
-    
     experimental = np.load(DIR_NPY+FILE_EXP,allow_pickle=True)
-    # strict_peaks = np.load(DIR_NPY+FILE_STRICT, allow_pickle=True)
+    strict_peaks = np.load(DIR_NPY+FILE_STRICT, allow_pickle=True)
     # Quick polar transform
     experimental = sm.fast_polar(experimental)
-    # strict_peaks = sm.fast_polar(strict_peaks)
+    strict_peaks = sm.fast_polar(strict_peaks)
     simulated = np.load(DIR_NPY+FILE_SIM,allow_pickle=True)
+    rot_simulated = np.load(DIR_NPY+FILE_SIM_ROT, allow_pickle=True)
     
     reciprocal_radius = 1.35 # [Å^-1]
     ang_step = 0.01
+    # ang_step = 1    # for quick testing
     exp_frame =56 
 
-    ## Check frame output
-    sim3d = np.array([sm.vector_to_3D(sim,reciprocal_radius) for sim in simulated])
-    # exp3d = np.array(sm.vector_to_3D(experimental[exp_frame],reciprocal_radius))
-    # match_one_frame(exp3d,sim3d, ang_step)
-    # print("----------")
+    ## n_best = 1 dataset creation ##
+    # n_best = 1 
+    # t1 = time()
+    # n1_array = sm.vector_match(experimental, rot_simulated, ang_step, reciprocal_radius, n_best)
+    # print(n1_array.shape)
+    # np.save(file=DIR_NPY+'ormap_n_best1_ang_step001.npy', arr=n1_array, allow_pickle=True)
+    # t2 = time()
+    # print(f"Computation time {(t2-t1)/60} min")
+    # print(f"Computation time n_best = 1 {(t2-t1)/60} min")
+
+    """ SETT PÅ DETTE NÅR DU KOMMER HJEM"""
+    ## n_best = len(simulated) dataset creation ##
+    n_best = len(simulated)
     t1 = time()
-    ang_step = 0.005
-    rot_sim_0005 = np.array([sm.full_z_rotation(sim, ang_step) for sim in sim3d])
-    np.save(file=DIR_NPY+'sim_rot_ang0005.npy', arr=rot_sim_0005, allow_pickle=True)
-    print(rot_sim_0005.shape)
+    n2_array = sm.vector_match(strict_peaks, rot_simulated, ang_step, reciprocal_radius, n_best)
+    # n2_array = sm.vector_match(experimental, rot_simulated, ang_step, reciprocal_radius, n_best)
+    print(n2_array.shape)
+    np.save(file=DIR_NPY+'ormap_strict_rad2deg_n_best_all_ang_step001.npy', arr=n2_array, allow_pickle=True)
     t2 = time()
-    print(f"Computation time {(t2-t1)/60} min")
+    print(f"Computation time file1: {(t2-t1)/60} min")
+
+    ## Dataset ang0005
+    ang_step = 0.005
+    rot_simulated = np.load(DIR_NPY+FILE_SIM_ROT2, allow_pickle=True)
+    n_best = len(simulated)
+    t1 = time()
+    n2_array = sm.vector_match(strict_peaks, rot_simulated, ang_step, reciprocal_radius, n_best)
+    print(n2_array.shape)
+    np.save(file=DIR_NPY+'ormap_strict_rad2deg_n_best_all_ang_step0005.npy', arr=n2_array, allow_pickle=True)
+    t2 = time()
+    print(f"Computation time file 2: {(t2-t1)/60} min")
+    """"""
 
 
 
