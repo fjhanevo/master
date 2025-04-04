86a1c47d429b09fe6ed08aa01d7e110bcb69ac94
fjhanevo <fredrik.hanevold@gmail.com>
2025-04-04 13:55:48 +0200

    added peak_find_one_frame and get_peaks

--- pre_processing/masking.py
+++ pre_processing/masking.py
@@ -3,6 +3,7 @@ import pyxem as pxm
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def get_center_interactive(s):
     """
     Creates a circle to mask out the center beam in a diffraction pattern.
@@ -48,21 +49,9 @@ def mask_center(s, radius, filename=None) -> None:
     if filename is not None:
         s.save(filename)
 
-
-def get_vectors(s,min_dist=20,threshold=0.2,intensity=False,filename=None) -> None:
-    """
-    Finds the diffraction peaks of a tilt series. 
-    min_dist and threshold are set to the default values after trial and error,hihi.
-    Optionally: Include the intensity, although it gives some weird values, 
-    and will give a (N,3) ndarray instead of a (N,2) ndarray, where N 
-    refers to the number of peaks found in each frame. 
-    Also save the file to a npy-file if you want to.
-    """
-    st = s.template_match_disk(disk_r=2.2, subtract_min=False)
-    vectors = st.get_diffraction_vectors(min_distance=min_dist,threshold_abs=threshold,
-                                         get_intensity=intensity)
-    if filename is not None:
-        np.save(file=filename,arr=vectors.data,allow_pickle=True)
+def log_shift(raw, base=10, shift=0.1):
+    log_shift = np.log10(raw+shift) - np.log10(shift)
+    return log_shift
 
 def mask_peaks(s,min_dist=20,threshold=0.2,intensity=False,filename=None) -> None:
     """
@@ -81,6 +70,34 @@ def mask_peaks(s,min_dist=20,threshold=0.2,intensity=False,filename=None) -> Non
     if filename is not None:
         s_masked.save(filename)
 
+def peak_find_one_frame(dp,frame, **kwargs):
+    st = dp.template_match_disk(disk_r=2.2, subtract_min=False)
+    dp_i = dp.inav[frame:frame+1]
+    st_i = st.inav[frame:frame+1]
+
+
+    vectors = st_i.get_diffraction_vectors(**kwargs)
+    # vm = vectors.to_mask(disk_r=4)
+    m = vectors.to_markers(sizes = 5, color='red')
+
+    print(vectors.data[0].shape)
+    dp_i.plot(cmap='viridis_r',norm='log',title='',colorbar=False,
+             scalebar=True,scalebar_color='black', axes_ticks='off')
+    dp_i.add_marker(m)
+
+    plt.show()
+    return vectors.data
+
+def get_peaks(dp, **kwargs):
+    """
+    Use if params has been specified to extract peaks from the entire dataset,
+    gives an inhomogeneous dataset with shapes (N,2) where N indicates the number
+    of peaks per frame.
+    """
+    st = dp.template_match_disk(disk_r = 2.2, subtract_min=False)
+    vectors = st.get_diffraction_vectors(**kwargs)
+    return vectors.data
+
 
 if __name__ == '__main__':
     DIR_HSPY = 'processed_hspy_files/'
@@ -89,43 +106,43 @@ if __name__ == '__main__':
     CENTER_FILE = 'LF_cal_log_m_center.hspy'
     SAVE_FILE = 'LF_cal_log_m_center_m_peaks.hspy'
     # VECTOR_FILE = 'LF_peaks_m_center_m_peaks.npy'
-    VECTOR_FILE = 'LF_peaks_masked_center.npy'
+    VECTOR_FILE = 'peaks_all_LoG.npy'
     # dp = hs.load(DIR_HSPY+FILE)
     dp = hs.load(DIR_HSPY+CENTER_FILE)
     # _,_,radius,_= get_center_interactive(dp)
     # print(radius)
     # radius = 0.26425
     # mask_center(dp,radius,DIR_HSPY+CENTER_FILE)
+    params_f29 = {
+        'method': 'laplacian_of_gaussian',
+        'get_intensity': False,
+        'min_sigma': 4,
+        'max_sigma': 5,
+        'num_sigma': 1,
+        'overlap': 0.1,
+        'log_scale': True,
+        'exclude_border': True
+    }
+    # file29 = 'f29_peaks.npy'
+    # f29_data = peak_find_one_frame(dp,29,**params_f29)
+    # np.save(file=DIR_NPY+file29, arr=f29_data, allow_pickle=True)
+    # print("File saved:",DIR_NPY+file29)
+    data = get_peaks(dp, **params_f29)
+    np.save(file=DIR_NPY+VECTOR_FILE, arr=data, allow_pickle=True)
+    print("File saved:",DIR_NPY+VECTOR_FILE)
+   
 
-    ###### Test out some vals for min_distance and threshold ########
-    # dist = 20
-    # threshold = 0.5
-    # get_vectors(dp, min_dist=dist,threshold=threshold, filename=DIR_NPY+VECTOR_FILE)
-    # mask_peaks(dp,min_dist=dist,threshold=threshold, filename=DIR_HSPY+SAVE_FILE)
-    # mask_peaks(dp)
-    st = dp.template_match_disk(disk_r=2.2, subtract_min=False)
-    vectors = st.get_diffraction_vectors(method='laplacian_of_gaussian',
-                                         get_intensity=False,
-                                         min_sigma=4.5,
-                                         max_sigma=50,
-                                         num_sigma=10,
-                                         overlap=0.1,
-                                         log_scale=False,
-                                         exclude_border=True)
-
-    # np.save(file=DIR_NPY+'LF_strict_peaks_log.npy',arr=vectors.data,allow_pickle=True)
-    # print(vectors.data.shape)
-    # print(vectors[56].data.shape)
-    vm =  vectors.to_mask(disk_r=4)
-    m =vectors.to_markers(sizes=5, color='red')
-
-    dp.plot(cmap='viridis_r',norm='log',title='',colorbar=False,
-             scalebar=True,scalebar_color='black', axes_ticks='off')
-    dp.add_marker(m)
-    plt.show()
-    # mask_peaks(dp)
-    # plot to see modified dataset
-    # s = hs.load(DIR_HSPY+SAVE_FILE)
-    # s.plot(cmap='viridis_r', norm='log', colorbar=False, scalebar_color='black')
-    # plt.show()
-
+    # params_f56= {
+    #     'method': 'laplacian_of_gaussian',
+    #     'get_intensity': False,
+    #     'min_sigma': 4.,
+    #     'max_sigma': 5,
+    #     'num_sigma': 1,
+    #     'overlap': 0.1,
+    #     'log_scale': True,
+    #     'exclude_border': True
+    # }
+    # file56 = 'f56_peaks.npy'
+    # f56_data = peak_find_one_frame(dp,56,**params_f56)
+    # np.save(file=DIR_NPY+file56, arr=f56_data, allow_pickle=True)
+    # print("File saved:",DIR_NPY+file56)
