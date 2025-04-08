be158790ad2a879d370241ca04447ae010accbea
fjhanevo <fredrik.hanevold@gmail.com>
2025-04-08 21:42:34 +0200

    added plot_misorientation_scatter

--- orientation_mapping/orientation_mapping.py
+++ orientation_mapping/orientation_mapping.py
@@ -5,6 +5,7 @@ from diffpy.structure import Atom, Lattice, Structure
 from diffsims.generators.simulation_generator import SimulationGenerator
 import matplotlib.pyplot as plt
 import numpy as np
+plt.rcParams.update({'font.size':18})
 
 def unit_cell(a=4.0495):
     atoms = [Atom('Al', [0,0,0]), Atom('Al', [0.5,0.5,0]),
@@ -43,7 +44,7 @@ def compute_simulations(simgen,phase,grid,reciprocal_radius=1.35,
         max_excitation_error=max_excitation_error,   # max excitation error, s
     )
 
-def plot_ipf(data, idx, phase, orientation):
+def plot_ipf(data, idx, phase, orientation,cmap:str):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='ipf', symmetry=phase.point_group)
 
@@ -52,10 +53,11 @@ def plot_ipf(data, idx, phase, orientation):
     orientations = orientation[tm_indices]
     loris= data.to_single_phase_orientations()
     loris_best = loris[idx,0]
-    ax.scatter(orientations, c=correlations, cmap='viridis_r')
+    ax.scatter(orientations, c=correlations, cmap=cmap)
     ax.scatter(loris_best,c='red',marker='o',s=100)
     plt.show()
 
+#NOTE: Denne funker ikke for 1 frame, gidder ikke fikse nå
 def plot_with_markers(results, file,i,j):
     """
     Plot markers on dataset
@@ -73,6 +75,27 @@ def plot_misorientation(data):
     loris_ang  = loris_best.angle_with_outer(loris_best, degrees=True)
     plt.figure()
     plt.hist(loris_ang.flatten(), bins=60)
+    plt.xlabel('Degrees')
+    plt.ylabel('Count')
+    plt.show()
+
+def plot_misorientation_scatter(data):
+    loris = data.to_single_phase_orientations()
+    loris_best = loris[:,0]
+    loris_ang = loris_best.angle_with_outer(loris_best,degrees=True)
+
+    plt.figure(figsize=(8,6))
+    for i in range(len(loris_ang)-1):
+        plt.scatter(i, loris_ang[i,i+1], s=34,c='black')
+
+    plt.axhline(y = 1, color='red', label=r'1$\degree$', linestyle='dashed')
+    plt.grid(True)
+    plt.ylabel(r'Misorientation$\degree$',fontsize='26')
+    plt.xlabel('Tilt Step',fontsize='26')
+    plt.xticks(fontsize='18')
+    plt.yticks(fontsize='18')
+    plt.legend(fontsize='18', loc='center left')
+    plt.tight_layout()
     plt.show()
 
 
@@ -91,9 +114,9 @@ def to_orientation_map(data, simulation):
 if __name__ == '__main__':
     DIR_NPY = 'npy_files/'
     DIR_HSPY = 'processed_hspy_files/'
-    HSPY = 'LF_cal_log_m_center_m_peaks.hspy'
+    HSPY = 'LF_cal_log_m_center_strict_peaks.hspy'
     ORG_HSPY = 'LeftFish_unmasked.hspy'
-    FILE = 'ormap_strict_rad2deg_n_best_all_ang_step0005.npy'
+    FILE = 'ormap_strict_ang1deg_n_best4186.npy'
 
     ### SIMULATED ###
     s = hs.load(DIR_HSPY+HSPY)
@@ -104,16 +127,24 @@ if __name__ == '__main__':
     simulation = compute_simulations(simgen, phase, grid, reciprocal_radius=1.35,
                                       max_excitation_error=0.05)
 
-    sim_results = s_pol.get_orientation(simulation,n_best=grid.size,frac_keep=1.)  # Creates an OrientationMap
-    frame = 56 
+    sim_results = s_pol.get_orientation(simulation,n_best=1,frac_keep=1.)  # Creates an OrientationMap
+    frame = 56
+    i, j = 29, 30
 
     ### EXPERIMENTAL ###
     exp_results = np.load(DIR_NPY+FILE, allow_pickle=True)
     exp_results = to_orientation_map(exp_results,simulation)
+    # print(sim_results.data[56][0])
+    plot_misorientation_scatter(exp_results)
+    plot_misorientation_scatter(sim_results)
 
-    # plot_ipf(exp_results,frame,phase,orientation)
-    plot_misorientation(sim_results)
-    plot_misorientation(exp_results)
+
+    # plot_ipf(exp_results,frame,phase,orientation, 'viridis_r')
+    # plot_ipf(sim_results,frame,phase,orientation, 'viridis')
+    # plot_with_markers(exp_results,DIR_HSPY+ORG_HSPY,i,j)
+    # plot_with_markers(sim_results,DIR_HSPY+ORG_HSPY,i,j)
+    # plot_misorientation(exp_results)
+    # plot_misorientation(sim_results)
 
     #### DETTE ER NYTTIG!!
     # xmap = results.to_crystal_map()
