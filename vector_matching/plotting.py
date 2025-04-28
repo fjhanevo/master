444c576b3e4c82c7ee2bb1c9abda4ba9296bc301
fjhanevo <fredrik.hanevold@gmail.com>
2025-04-28 11:41:41 +0200

    added all plotting functionality from orientation_mapping.py

--- vector_matching/plotting.py
+++ vector_matching/plotting.py
@@ -1,5 +1,9 @@
+import hyperspy.api as hs
+from orix.plot import IPFColorKeyTSL
+from orix.vector import Vector3d
 import matplotlib.pyplot as plt
 import numpy as np
+plt.rcParams.update({'font.size':18})
 
 """
 Fil for plottefunksjoner
@@ -147,3 +151,86 @@ def plot_2D_plane_save(s1:np.ndarray, s2:np.ndarray, labels:tuple) -> None:
 
     plt.legend()
     plt.savefig('vector_matching/sphere_gif/'+filename)
+
+
+### Orientation Mapping plots ###
+def plot_ipf(data, idx, phase, orientation ,cmap:str):
+    """
+    Plots an IPF with a red marker indicating the best found orientation.
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='ipf', symmetry=phase.point_group)
+
+    correlations = data.inav[idx].data[:,1]
+    tm_indices = (data.inav[idx].data[:,0]).astype('int16')
+    orientations = orientation[tm_indices]
+    loris= data.to_single_phase_orientations()
+    loris_best = loris[idx,0]
+    ax.scatter(orientations, c=correlations, cmap=cmap)
+    ax.scatter(loris_best,c='red',marker='o',s=100) # best found orientation
+    plt.show()
+
+def plot_with_markers(results, file,i,j):
+    """
+    Plot markers on dataset
+    """
+    data = hs.load(file)
+    data = data.inav[i:j]
+    results = results.inav[i:j]
+    data.plot(cmap='viridis_r', norm='log', title='', colorbar=False, scalebar_color='black', axes_ticks='off')
+    data.add_marker(results.to_markers(annotate=True))
+    plt.show()
+
+def plot_misorientation_hist(data):
+    loris = data.to_single_phase_orientations()
+    loris_best = loris[:,0]
+    loris_ang  = loris_best.angle_with_outer(loris_best, degrees=True)
+    plt.figure()
+    plt.hist(loris_ang.flatten(), bins=60)
+    plt.xlabel('Degrees')
+    plt.ylabel('Count')
+    plt.show()
+
+def plot_misorientation_scatter(data):
+    loris = data.to_single_phase_orientations()
+    loris_best = loris[:,0]
+    loris_ang = loris_best.angle_with_outer(loris_best,degrees=True)
+
+    plt.figure(figsize=(8,6))
+    for i in range(len(loris_ang)-1):
+        plt.scatter(i, loris_ang[i,i+1], s=34,c='black')
+
+    plt.axhline(y = 1, color='red', label=r'1$\degree$', linestyle='dashed')
+    plt.grid(True)
+    plt.ylabel(r'Misorientation$\degree$',fontsize='26')
+    plt.xlabel('Tilt Step',fontsize='26')
+    plt.xticks(fontsize='18')
+    plt.yticks(fontsize='18')
+    plt.legend(fontsize='18', loc='center left')
+    plt.tight_layout()
+    plt.show()
+
+def plot_crystal_map(results,phase):
+    """
+    Assumes reshaped dataset!
+    """
+    xmap = results.to_crystal_map()
+    oris = xmap.orientations
+    corrs = results.data[:,:,0,1].flatten()
+    print(corrs.shape)
+
+    key_x = IPFColorKeyTSL(phase.point_group, Vector3d.xvector())
+    key_y = IPFColorKeyTSL(phase.point_group, Vector3d.yvector())
+    key_z = IPFColorKeyTSL(phase.point_group, Vector3d.zvector())
+
+    oris_z = key_z.orientation2color(oris)[:,:]
+    xmap.plot(oris_z, overlay=corrs, remove_padding=True)
+    plt.show()
+    oris_x = key_x.orientation2color(oris)[:,:]
+    xmap.plot(oris_x, overlay=corrs, remove_padding=True)
+    plt.show()
+    oris_y = key_y.orientation2color(oris)[:,:]
+    xmap.plot(oris_y, overlay=corrs, remove_padding=True)
+    plt.show()
+
+
