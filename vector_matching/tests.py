bcbaea047711464116d3669fe5701c4d99ccd804
fjhanevo <fredrik.hanevold@gmail.com>
2025-05-08 17:32:55 +0200

    started on algorithm_accuracy to determine accuracy of vm and tm by matching with themselves

--- vector_matching/tests.py
+++ vector_matching/tests.py
@@ -1,16 +1,39 @@
 import numpy as np
+import pyxem as pxm
 from scipy.spatial import cKDTree
-from sphere_matching import vector_to_3D, filter_sim, apply_z_rotation
+from sphere_matching import vector_to_3D, filter_sim, apply_z_rotation, wrap_degrees, vector_match_ang_score
 from time import time
 import plotting
+import simulation as sim
 
+def algorithm_accuracy(data, stepsize, reciprocal_radius, n_best):
+    """
+    Use VM and TM to match with itself.
+    """
+    tm_data_to_obj = pxm.signals.Diffraction2D(data)
+    s_pol = tm_data_to_obj.get_azimuthal_integral2d(npt=112, radial_range=(0.,1.35))
+    phase = sim.unit_cell()
+    grid, orientation = sim.gen_orientation_grid(phase)
+
+    simgen = sim.get_simulation_generator(precession_angle=1., minimum_intensity=1e-4, approximate_precession=True)
+    simulation = sim.compute_simulations(simgen, phase, grid, reciprocal_radius=1.35,
+                                      max_excitation_error=0.05)
+
+    ### This hopefully template matches itself
+    template_matching = s_pol.get_orientation(simulation,n_best=n_best,frac_keep=1.) 
+
+    vector_matching = vector_match_ang_score(data, data, stepsize,reciprocal_radius,n_best) 
+    
+
+
+#NOTE: Slett denne
 def vector_match_score_test(
     experimental:np.ndarray, 
     simulated:np.ndarray, 
     step_size:float, 
     reciprocal_radius:float,
     distance_bound=0.05,
-    unmatched_penalty=0.75
+    unmatched_penalty=1.0
 ) -> np.ndarray:
     """
     Match only one frame
@@ -29,8 +52,7 @@ def vector_match_score_test(
 
     # to 3D and mirror
     exp3d = vector_to_3D(experimental, reciprocal_radius)
-    #NOTE: Endrer denne kjapt til å fungere for intensities.
-    exp3d_mirror = exp3d * np.array([-1,1,1,1])
+    exp3d_mirror = exp3d * np.array([-1,1,1])
     results =[]
 
     for sim_idx, trees in enumerate(precomputed_trees):
@@ -40,6 +62,8 @@ def vector_match_score_test(
             # Points in current sim frame
             sim_points = sim_tree.data
 
+            # total points 
+            n_tot = len(exp3d) + len(sim_points)
             # Experimental tree
             exp_tree = cKDTree(exp3d)
             exp_tree_mirror = cKDTree(exp3d_mirror)
@@ -48,11 +72,12 @@ def vector_match_score_test(
             dist_exp_to_sim, _ = sim_tree.query(exp3d,distance_upper_bound=distance_bound)
             dist_sim_to_exp, _ = exp_tree.query(sim_points,distance_upper_bound=distance_bound)
 
-            #NOTE: Endrer score til å ta hensyn for intensities
+
             n_unmatched_exp = np.sum(np.isinf(dist_exp_to_sim))
             n_unmatched_sim = np.sum(np.isinf(dist_sim_to_exp))
             matched_score = np.sum(dist_exp_to_sim[np.isfinite(dist_exp_to_sim)])
-            score = matched_score + unmatched_penalty * (n_unmatched_exp + n_unmatched_sim)
+            # normalise the score
+            score = (matched_score + unmatched_penalty * (n_unmatched_exp + n_unmatched_sim))/n_tot
 
             # Mirrored version
             dist_exp_to_sim_m, _ = sim_tree.query(exp3d_mirror,distance_upper_bound=distance_bound)
@@ -61,17 +86,20 @@ def vector_match_score_test(
             n_unmatched_exp_m = np.sum(np.isinf(dist_exp_to_sim_m))
             n_unmatched_sim_m = np.sum(np.isinf(dist_sim_to_exp_m))
             matched_score_m = np.sum(dist_exp_to_sim_m[np.isfinite(dist_exp_to_sim_m)])
-            score_mirror = matched_score_m + unmatched_penalty * (n_unmatched_exp_m + n_unmatched_sim_m)
+            # normalise score_mirror
+            score_mirror = (matched_score_m + unmatched_penalty * (n_unmatched_exp_m + n_unmatched_sim_m))/n_tot
 
 
             if score < best_score:
                 best_score = score
-                best_rotation = np.rad2deg(rot_idx * step_size_rad)
+                ang = rot_idx * step_size_rad 
+                best_rotation = wrap_degrees(ang) 
                 mirror = 1.0
 
             if score_mirror < best_score:
                 best_score = score_mirror
-                best_rotation = np.rad2deg(rot_idx * step_size_rad)
+                ang = rot_idx * step_size_rad 
+                best_rotation = wrap_degrees(ang) 
                 mirror = -1.0
         results.append((sim_idx, best_score, best_rotation, mirror))
     # results = sorted(results, key = lambda x : x[1])[:n_best]
@@ -81,31 +109,4 @@ def vector_match_score_test(
     n_array = np.array(result_lst)
     return n_array
      
-
-def test_params_and_save(exp, exp_frame,sim, rot, reciprocal_radius, dist_bound, penalty):
-    exp = exp[exp_frame]
-    # n_array = sm.vm_one_frame_take_two(exp,sim,ang_step,reciprocal_radius, n_best_candidates=n_best)
-    n_array = vector_match_score_test(
-        exp,sim,rot,reciprocal_radius,
-        distance_bound=dist_bound, unmatched_penalty=penalty
-    )
-    n_best = n_array[0][0]
-    frame, score, rotation, mirror = n_best[0],n_best[1],n_best[2], n_best[3]
-
-    exp3d = vector_to_3D(exp,reciprocal_radius)
-    rot = np.deg2rad(rot)
-    if mirror < 0.0:
-        exp3d *= np.array([-1,1,1])
-    sim = sim[int(frame)]
-    sim_filtered = sim[~np.all(sim==0,axis=1)]
-    sim_filtered3d = vector_to_3D(sim_filtered, reciprocal_radius)
-    sim_filtered3d_rot = np.array([apply_z_rotation(vec,rot) for vec in sim_filtered3d])
-    sim_str = 'sim['+str(int(frame))+']'
-    exp_str = 'exp['+str(exp_frame)+']'
-    dir = 'vector_matching/tmp_plots/'
-    filename = dir+'f'+str(exp_frame)+'_dist'+str(dist_bound)+'_pen'+str(penalty)+'.png'
-    lbls = (sim_str, exp_str, filename)
-    
-    plotting.plot_save_spheres(sim_filtered3d_rot, exp3d, lbls)
-
 
