7b0d46e1ded069e707989061e103c531aba637c5
fjhanevo <fredrik.hanevold@gmail.com>
2025-05-10 14:57:28 +0200

    fixed a bug in compare_orientations

--- vector_matching/orientation_mapping.py
+++ vector_matching/orientation_mapping.py
@@ -41,7 +41,7 @@ def compare_orientations(tm_orientation:np.ndarray, vm_orientation:np.ndarray) -
     diff = np.where(diff > 180, 360 - diff, diff)   # take shortest angle
 
     # Optionally: check simulation indices too
-    sim_match = tm_orientation[:,:, 0] == vm_orientation[:,[0],0]
+    sim_match = tm_orientation[:,:, 0] == vm_orientation[:,:,0]
 
     # Stats
     stats = {
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     DIR_HSPY = 'processed_hspy_files/'
     HSPY = 'LF_cal_log_m_center_strict_peaks.hspy'
     ORG_HSPY = 'LeftFish_unmasked.hspy'
-    FILE = 'ormap_step05deg_dist005_penalty075.npy'
+    FILE = '060525ormap_step05deg_vector_match_ang_score_NO_MIRROR_wrap_degrees_v060525.npy'
 
     s = hs.load(DIR_HSPY+HSPY)
     ### UNCOMMENT FOR crystal_map ### 
@@ -92,17 +92,27 @@ if __name__ == '__main__':
                                       max_excitation_error=0.05)
 
     sim_results = s_pol.get_orientation(simulation,n_best=grid.size,frac_keep=1.)  # Creates an OrientationMap
-    frame = 29
+    print(type(s_pol))
+    frame = 56
     i, j = frame, frame+1
     
     ### EXPERIMENTAL ###
     exp_results = np.load(DIR_NPY+FILE, allow_pickle=True)
     exp_results = to_orientation_map(exp_results,simulation)
+    exp2 = np.load(DIR_NPY+'060525ormap_step05deg_vector_match_NO_MIRROR_wrap_degrees_v060525.npy', allow_pickle=True)
+    exp2 = to_orientation_map(exp2,simulation)
+    # print(compare_orientations(exp_results.data, exp2.data))
+    # print(exp_results.data[frame][0])
+    # print(exp2.data[frame][0])
+    
 
-    print(exp_results.data[56][0])
     # stats = compare_orientations(sim_results.data, exp_results.data)
     # print(stats)
 
+    # exp_results.data[frame][0][2] += 107.64028464817187 + 90
+    # exp_results.data[frame][0][3] = 0
+    # exp_results.data[frame][0][0] = 439
+    # sim_results.data[frame][0][0] = exp_results.data[frame][0][0]
     ### PLOTS ### 
     # plot.plot_misorientation_scatter(exp_results)
     # plot.plot_misorientation_scatter(sim_results)
