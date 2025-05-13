9657c9822b073479fc1caf5040575ce0138424ce
fjhanevo <fredrik.hanevold@gmail.com>
2025-05-13 15:31:26 +0200

    added check_overlay_plot for quicker plots while working on mirroring

--- vector_matching/orientation_mapping.py
+++ vector_matching/orientation_mapping.py
@@ -4,6 +4,8 @@ import numpy as np
 import simulation as sim
 import plotting as plot
 import matplotlib.pyplot as plt
+from sphere_matching import vector_match, fast_polar
+from time import time
 
 
 def to_orientation_map(data, simulation):
@@ -63,6 +65,31 @@ def compare_orientations(tm_orientation:np.ndarray, vm_orientation:np.ndarray) -
 
     return stats 
 
+def check_overlay_plot(exp, frame, sim, simulation):
+    # Match one frame
+    t1 = time()
+    n_array = vector_match([exp[frame]], sim,step_size=0.5, reciprocal_radius=1.35, n_best=1)
+    t2 = time()
+    print(n_array.shape)
+    n_best = n_array[0][0]
+    frame, score, rotation, mirror = n_best[0],n_best[1],n_best[2], n_best[3]
+    print(f"Computation time: {(t2-t1)/60} min")
+    print('Best frame:', frame)
+    print('Best score:',score) 
+    print('Best rotation:', rotation)
+    print('Mirror factor:', mirror)
+
+    n_ormap = to_orientation_map(n_array, simulation)
+    # load org dataset
+    s = hs.load('processed_hspy_files/LeftFish_unmasked.hspy')
+    # Take out the frame
+    s_frame = s.inav[frame:frame+1]
+
+    s_frame.plot(cmap='viridis_r', norm='log', title='', colorbar=False, scalebar_color='black', axes_ticks='off')
+    s_frame.add_marker(n_ormap.to_markers(annotate=True))
+    plt.show()
+
+
 
 if __name__ == '__main__':
     DIR_NPY = 'npy_files/'
@@ -83,6 +110,7 @@ if __name__ == '__main__':
     # print(s.axes_manager.navigation_axes[0].units)
     # ------------------------------------------ #
 
+
     ### SIMULATED ###
     s_pol = s.get_azimuthal_integral2d(npt=112, radial_range=(0.,1.35))
     phase = sim.unit_cell()
@@ -91,19 +119,26 @@ if __name__ == '__main__':
     simulation = sim.compute_simulations(simgen, phase, grid, reciprocal_radius=1.35,
                                       max_excitation_error=0.05)
 
-    sim_results = s_pol.get_orientation(simulation,n_best=grid.size,frac_keep=1.)  # Creates an OrientationMap
-    print(type(s_pol))
+    # sim_results = s_pol.get_orientation(simulation,n_best=grid.size,frac_keep=1.)  # Creates an OrientationMap
+
+    ### Test check_overlay_plot ### 
+    FILE_STRICT = 'peaks_all_LoG.npy'
+    FILE_SIM = 'LF_r_theta_sim.npy'
+    experimental = np.load(DIR_NPY+FILE_STRICT,allow_pickle=True)
+    # Quick polar transform
+    experimental = fast_polar(experimental)
+    simulated = np.load(DIR_NPY+FILE_SIM,allow_pickle=True)
+
     frame = 56
+    check_overlay_plot(experimental,frame, simulated, simulation)
+
+
     i, j = frame, frame+1
     
     ### EXPERIMENTAL ###
-    exp_results = np.load(DIR_NPY+FILE, allow_pickle=True)
-    exp_results = to_orientation_map(exp_results,simulation)
-    exp2 = np.load(DIR_NPY+'060525ormap_step05deg_vector_match_NO_MIRROR_wrap_degrees_v060525.npy', allow_pickle=True)
-    exp2 = to_orientation_map(exp2,simulation)
-    # print(compare_orientations(exp_results.data, exp2.data))
-    # print(exp_results.data[frame][0])
-    # print(exp2.data[frame][0])
+    # exp_results = np.load(DIR_NPY+FILE, allow_pickle=True)
+    # exp_results = to_orientation_map(exp_results,simulation)
+
     
 
     # stats = compare_orientations(sim_results.data, exp_results.data)
