985307e15001b04fdfed99230840d9287bb7d446
fjhanevo <fredrik.hanevold@gmail.com>
2025-05-23 14:35:33 +0200

    merged vector_match_parallelized into vector_match added fast parameter, changed name of process_exp_frame to process_frames

--- vector_matching/vector_match.py
+++ vector_matching/vector_match.py
@@ -117,6 +117,8 @@ def vector_match(
     reciprocal_radius: float,
     n_best: int,
     method: str = "sum",
+    fast: bool = False,
+    n_jobs: int = -1,
     distance_bound: float = 0.05,
     dtype=np.float32
 ) -> np.ndarray:
@@ -124,52 +126,50 @@ def vector_match(
     Docstring
     """
     
+    valid_methods = {"sum_score", "sum_score_weighted"}
+    if method not in valid_methods:
+        raise ValueError(f"Unsupported method: {method}. Valid options: {valid_methods}")
+
     # Convert input degrees to radians
     step_size_rad = np.deg2rad(step_size)
-    if method == "sum_score" or method == "sum_score_weighted":
-        # Precompute KD-trees for rotated simulated frames
-        precomputed_data= [
-            [cKDTree(rot_frame) for rot_frame in vm_utils.filter_sim(sim_frame, step_size_rad, reciprocal_radius,dtype)]
-            for sim_frame in simulated
-        ]
-    else:
-        raise ValueError(f"Unsupported method: {method}")
+    # Precompute KD-trees for rotated simulated frames
+    precomputed_data= [
+        [cKDTree(rot_frame[:, :3]) for rot_frame in vm_utils.filter_sim(sim_frame, step_size_rad, reciprocal_radius,dtype)]
+        for sim_frame in simulated
+    ]
 
     # array to store final results
     n_array = []
-    
     # Pre-compute exp3d and its mirror
     exp3d_all = [vm_utils.vector_to_3D(exp_vec, reciprocal_radius,dtype) for exp_vec in experimental]
     exp3d_mirror_all = [exp_vec * np.array([1,-1,1], dtype=dtype) for exp_vec in exp3d_all]
 
     # Loop through experimental vectors
-    for idx in tqdm(range(len(experimental))):
+    if fast:
+        n_array= Parallel(n_jobs=n_jobs) (
+        delayed(process_frames) (
+            exp3d=exp3d_all[idx],
+            exp3d_mirror=exp3d_mirror_all[idx],
+            sim_data=precomputed_data,
+            step_size_rad=step_size_rad,
+            n_best=n_best,
+            method=method,
+            distance_bound=distance_bound
+        ) for idx in tqdm(range(len(experimental)))
+    )
+        return np.stack(n_array)
+    else:
+        for idx in tqdm(range(len(experimental))):
 
-        # array to store results per iteration of shape (n_best, 4)
-        iteration_results = []
-        
-        for sim_idx, sim_tree_rotated in enumerate(precomputed_data):
-            best_score, best_rotation, mirror = np.inf, 0.0, 0
-
-            if method == "sum_score":
-                best_score, best_rotation, mirror = sum_score(
-                    exp3d_all[idx], exp3d_mirror_all[idx], sim_tree_rotated, step_size_rad)
-
-            elif method == "sum_score_weighted":
-                best_score, best_rotation, mirror = sum_score_weighted(
-                    exp3d_all[idx], exp3d_mirror_all[idx], sim_tree_rotated, step_size_rad,distance_bound)
-            iteration_results.append((sim_idx, best_score, best_rotation, mirror))
+            n_array.append(process_frames(
+                exp3d_all[idx], exp3d_mirror_all[idx], precomputed_data, step_size_rad, n_best,
+                method, distance_bound
+            ))
+        # returns nx4 array of shape (len(experimental), n_best, 4)
+        return np.stack(n_array)
 
 
-        # append results and sort by ascending score
-        n_array.append(heapq.nsmallest(n_best, iteration_results, key=lambda x: x[1]))
-            
-    # returns nx4 array of shape (len(experimental), n_best, 4)
-    return np.stack(n_array)
-
-
-#NOTE: Used for parallelization
-def process_exp_frame(
+def process_frames(
     exp3d: np.ndarray,
     exp3d_mirror: np.ndarray,
     sim_data,
@@ -181,6 +181,7 @@ def process_exp_frame(
     iteration_results = []
 
     for sim_idx, sim_tree_rotated in enumerate(sim_data):
+        best_score, best_rotation, mirror = np.inf, 0.0, 0
         if method == "sum":
             best_score, best_rotation, mirror = sum_score(
                 exp3d, exp3d_mirror, sim_tree_rotated, step_size_rad
@@ -189,55 +190,8 @@ def process_exp_frame(
             best_score, best_rotation, mirror = sum_score_weighted(
                 exp3d, exp3d_mirror, sim_tree_rotated, step_size_rad, distance_bound
             )
-        else:
-            raise ValueError(f"Unsupported method {method}")
         iteration_results.append((sim_idx, best_score, best_rotation, mirror))
 
     return heapq.nsmallest(n_best, iteration_results, key=lambda x: x[1])
 
              
-#NOTE: This function is logically similar to vector_match but 
-# attempts to use more of the CPU and memory for faster runtime.
-# it is very RAM demanding
-def vector_match_parallelized(
-    experimental: np.ndarray,
-    simulated: np.ndarray,
-    step_size: float, 
-    reciprocal_radius: float,
-    n_best: int,
-    method: str = "sum",
-    n_jobs: int = -1,
-    distance_bound: float = 0.05,
-    dtype=np.float32,
-) -> np.ndarray:
-    """
-    Docstring
-    """
-    
-    # Convert input degrees to radians
-    step_size_rad = np.deg2rad(step_size)
-    if method == "sum" or method == "sum_score_weighted":
-        # Precompute KD-trees for rotated simulated frames
-        precomputed_data= [
-            [cKDTree(rot_frame) for rot_frame in vm_utils.filter_sim(sim_frame, step_size_rad, reciprocal_radius,dtype)]
-            for sim_frame in simulated
-        ]
-    else:
-        raise ValueError(f"Unsupported method {method}")
-
-    # precompute experimental projections and mirrors
-    exp3d_all = [vm_utils.vector_to_3D(exp_vec, reciprocal_radius,dtype) for exp_vec in experimental]
-    exp3d_mirror_all = [exp_vec * np.array([1,-1,1], dtype=dtype) for exp_vec in exp3d_all]
-
-    results = Parallel(n_jobs=n_jobs) (
-        delayed(process_exp_frame) (
-            exp3d=exp3d_all[idx],
-            exp3d_mirror=exp3d_mirror_all[idx],
-            sim_data=precomputed_data,
-            step_size_rad=step_size_rad,
-            n_best=n_best,
-            method=method,
-            distance_bound=distance_bound
-        ) for idx in tqdm(range(len(experimental)))
-    )
-    return np.stack(results)
